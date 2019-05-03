f09b1d232c6744cd92d04fd1b3f985932aa76703
Gea-Suan Lin <gslin@gslin.org>
2019-05-04 01:04:51 +0800

    Refactor.

--- show-twitter-list.user.js
+++ show-twitter-list.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show Twitter List
 // @namespace    https://wiki.gslin.org/wiki/ShowTwitterList
-// @version      0.0.20180818.0
+// @version      0.0.20190504.0
 // @description  Show twitter list in title.
 // @author       Gea-Suan Lin <darkkiller@gmail.com>
 // @match        https://twitter.com/*
@@ -16,7 +16,7 @@
     let profile_banner_re = new RegExp('https://pbs\.twimg\.com/profile_banners/([0-9]+)/');
     let url_re = new RegExp('^https://twitter\.com/[^/]+(/media)?$');
 
-    let main = function(){
+    (() => {
         if (!document.location.href.match(url_re)) {
             return;
         }
@@ -43,7 +43,5 @@
         });
         req.open('GET', url);
         req.send();
-    };
-
-    main();
+    })();
 })();
